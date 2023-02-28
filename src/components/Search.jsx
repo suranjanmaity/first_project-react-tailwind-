c0bdf8f6019045bccb702125e8e3a45eed4b3234
suranjanmaity <suranjanmaity4@gmail.com>
2023-02-28 18:40:18 +0530

    intial commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,32 @@
 const Search = ({ data }) => {
     return (
       <section className="main-block">
-        <article>
-          <h1>Search Country</h1>
+        <article className="flex flex-col items-center justify-center
+        bg-gradient-to-r from-purple-500 to-pink-500
+        h-screen
+        ">
+          <h1 className="
+          text-7xl text-rose-300 hover:text-fuchsia-300
+          font-sans 
+          border-8 hover:border-dashed rounded-3xl
+          py-3.5
+          transition ease-in-out delay-100 bg-purple-500/[0.7]
+          hover:-translate-y-3 hover:scale-110 hover:bg-pink-500/[0.7] duration-300
+          ">Search Country</h1>
           <form>
-            {/* <input type="text" placeholder="search country...." list="country" />
-          <datalist id="country">
-            {data.map(value => {
-              return <option key={value.code} value={value.name} />;
-            })}
-          </datalist> */}
-  
-            <select>
+            <select className="
+            h-10 w-86
+            rounded-lg
+            font-mono
+            transition ease-in-out delay-100
+            hover:translate-y-2 hover:bg-black hover:text-white
+            ">
               {data.map(value => {
                 return (
-                  <option key={value.code} value={value.name}>
+                  <option className="
+                  font-mono
+                  border-none
+                  " key={value.code} value={value.name}>
                     {value.name}
                   </option>
                 );
